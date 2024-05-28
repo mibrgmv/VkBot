00d9b78dcbc57108769abd2b00b1e80293cf464e
mibrgmv <mibrgmv@gmail.com>
2024-05-28 22:21:16 +0300

    initial

---
+++ src/main/java/ru/vkbot/VkBotApplication.java
@@ -0,0 +1,13 @@
+package ru.vkbot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkBotApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(VkBotApplication.class, args);
+        }
+    }
+
+}
